61aceaca8a112a123eec7d9b6a069edfc9b1c858
Kim-dong-young <rlaehddud9502@naver.com>
2024-12-19 23:03:19 +0900

    문제 출처 추가

--- codingtest/src/codingtest/programmers/lv1/Test241219_1.java
+++ codingtest/src/codingtest/programmers/lv1/Test241219_1.java
@@ -1,5 +1,7 @@
 package codingtest.programmers.lv1;
 
+// [PCCE 기출문제] 9번 / 지폐 접기
+// https://school.programmers.co.kr/learn/courses/30/lessons/340199
 public class Test241219_1 {
 	public static void main(String[] args) {
 		int answer = 0;
