9c49dbceff69d7ef94702591f7ae9fbbd7212c83
Kim-dong-young <rlaehddud9502@naver.com>
2024-12-28 23:55:40 +0900

    2024/12/28 코테 푸는중

--- codingtest/src/codingtest/programmers/lv2/Test241227_1.java
+++ codingtest/src/codingtest/programmers/lv2/Test241227_1.java
@@ -1,7 +1,6 @@
 package codingtest.programmers.lv2;
 
 // [PCCP 기출문제] 3번 / 충돌위험 찾기
-
 // https://school.programmers.co.kr/learn/courses/30/lessons/340211
 public class Test241227_1 {
 	public static void main(String[] args) {
@@ -9,9 +8,10 @@ public class Test241227_1 {
 		int[][] points = {{3,2},{6,4},{4,7},{1,4}};
 		int[][] routes = {{4,2},{1,3},{2,4}};
 		
+		int result = 0;
+		
 		Robot[] robots = new Robot[routes.length];
 		
-		// 항상 최단 경로로 이동, y 좌표 우선 이동 후, x 좌표 이동
 		// => 이동하는 로봇 객체 만들어보자
 		for(int i=0; i < routes.length; i++) {
 			int[][] destinations = new int[routes[0].length][2];
@@ -23,9 +23,55 @@ public class Test241227_1 {
 			robots[i] = new Robot(destinations);
 		}
 		
-		for(Robot robot : robots) {
-			System.out.println(robot);
+		int index = 1;
+		while(true) {
+			System.out.println("============"+ index +"번째 =============");
+			index ++;
+			
+			for(Robot robot : robots) {
+				System.out.println(robot);
+			}
+			
+			boolean[][] isChecked = new boolean[robots.length][robots.length];
+			for(int i=0; i < robots.length; i++) {
+				for(int j=0; j < robots.length; j++) {
+					
+					if(i == j) continue;
+					
+					if(robots[i].equals(robots[j]) && 
+						!(isChecked[i][j] || isChecked[j][i])) {
+						isChecked[i][j] = true;
+						result++;
+					}
+				}
+			}
+			
+			System.out.println("\nResult : " + result + "\n");
+			
+			for(Robot robot : robots) {
+				robot.move();
+			}
+			
+			System.out.println("\n움직인 후 로봇\n");
+			
+			for(Robot robot : robots) {
+				System.out.println(robot);
+			}
+			
+			System.out.println("==========================");
+			
+			boolean isAllFinish = true;
+			for(Robot robot : robots) {
+				if(!robot.isEnd()) {
+					isAllFinish = false;
+					break;
+				}
+			}
+			
+			if(isAllFinish) break;
 		}
+		
+		System.out.println(result);
 	}
 }
 
@@ -33,11 +79,39 @@ class Robot{
 	private int x;
 	private int y;
 	private int[][] destinations;
+	private int dIndex;
 	
 	public Robot(int[][] destinations) {
 		this.x = destinations[0][0];
 		this.y = destinations[0][1];
 		this.destinations = destinations;
+		this.dIndex = 1;
+	}
+	
+	public void move() {
+		if(dIndex >= destinations.length) {
+			x = -1;
+			y = -1;
+			return;
+		}
+		
+		// 항상 최단 경로로 이동, y 좌표 우선 이동 후, x 좌표 이동
+		if(x != destinations[dIndex][0]) {
+			if(x < destinations[dIndex][0]) x++;
+			if(x > destinations[dIndex][0]) x--;
+		}
+		else if(y != destinations[dIndex][1]) {
+			if(y < destinations[dIndex][1]) y++;
+			if(y > destinations[dIndex][1]) y--;
+		}
+		
+		if(x == destinations[dIndex][0] && y == destinations[dIndex][1]) {
+			dIndex++;
+		}
+	}
+	
+	public boolean isEnd() {
+		return dIndex >= destinations.length;
 	}
 
 	@Override
@@ -46,7 +120,7 @@ class Robot{
 		for(int[] destination : destinations) {
 			dstToString += " [ " + destination[0] + "," + destination[1] + " ] ";
 		}
-		return "Robot [x=" + x + ", y=" + y + ", destination=" + dstToString + "]";
+		return "Robot [x=" + x + ", y=" + y + ", dIndex="+ dIndex +" , destination=" + dstToString + "]";
 	}
 
 	@Override
