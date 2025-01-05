db676d71f1f780c22ee82f83f581d1eb56b76cb4
Kim-dong-young <rlaehddud9502@naver.com>
2025-01-05 23:40:08 +0900

    2025/1/5 코테푸는중 효율성 통과실패

--- codingtest/src/codingtest/programmers/lv2/Test241231_1.java
+++ codingtest/src/codingtest/programmers/lv2/Test241231_1.java
@@ -4,62 +4,101 @@ package codingtest.programmers.lv2;
 // https://school.programmers.co.kr/learn/courses/30/lessons/250136
 public class Test241231_1 {
 	public static void main(String[] args) {
+		/*
+			 {
+				 {1, 0, 1, 0, 1, 1}, 
+				 {1, 0, 1, 0, 0, 0}, 
+				 {1, 0, 1, 0, 0, 1}, 
+				 {1, 0, 0, 1, 0, 0}, 
+				 {1, 0, 0, 1, 0, 1}, 
+				 {1, 0, 0, 0, 0, 0}, 
+				 {1, 1, 1, 1, 1, 1}
+			 }
+			 
+			 {
+				{0, 0, 0, 1, 1, 1, 0, 0}, 
+				{0, 0, 0, 0, 1, 1, 0, 0}, 
+				{1, 1, 0, 0, 0, 1, 1, 0}, 
+				{1, 1, 1, 0, 0, 0, 0, 0}, 
+				{1, 1, 1, 0, 0, 0, 1, 1}
+			}
+		 */
 		int[][] land = {
-			{0, 0, 0, 1, 1, 1, 0, 0}, 
-			{0, 0, 0, 0, 1, 1, 0, 0}, 
-			{1, 1, 0, 0, 0, 1, 1, 0}, 
-			{1, 1, 1, 0, 0, 0, 0, 0}, 
-			{1, 1, 1, 0, 0, 0, 1, 1}
-		};
+				 {1, 0, 1, 0, 1, 1}, 
+				 {1, 0, 1, 0, 0, 0}, 
+				 {1, 0, 1, 0, 0, 1}, 
+				 {1, 0, 0, 1, 0, 0}, 
+				 {1, 0, 0, 1, 0, 1}, 
+				 {1, 0, 0, 0, 0, 0}, 
+				 {1, 1, 1, 1, 1, 1}
+		 };
 		
-		int[] expectOil = new int[land.length];
-		boolean[][] isVisited = new boolean[land.length][land[0].length];
+		int[] expectOil = new int[land[0].length];
 		
 		// 시추 포인트
 		for(int sichu=0; sichu < land[0].length; sichu++) {
+			boolean[][] isVisited = new boolean[land.length][land[0].length];
 			// 밑으로 시추
 			for(int depth=0; depth < land.length; depth++) {
 				// 그냥 땅이거나, 이미 측정한 땅이면 지나침
 				if(land[depth][sichu] == 0 || isVisited[depth][sichu]) continue;
-				
+//				System.out.println("depth : " + depth + " sichu : " + sichu);
 				// 1. 석유일 경우 석유의 개수 세기
 				expectOil[sichu] += countOil(land, isVisited, depth, sichu);
 			}
+			
+//			for(int i=0; i < land.length; i++) {
+//				for(int j=0; j < land[0].length; j++) {
+//					System.out.print(String.format("%6b", isVisited[i][j]));
+//				}
+//				System.out.println("");
+//			}
+//			System.out.println();
 		}
 		
+//		for(int oilCnt : expectOil) {
+//			System.out.print(oilCnt + " ");
+//		}
+		int max = expectOil[0];
 		for(int oilCnt : expectOil) {
-			System.out.println(oilCnt);
+			max = Math.max(max, oilCnt);
 		}
+		
+		System.out.println(max);
 	}
 	
-	public static int countOil(int[][] land, boolean[][]isVisited, int x, int y) {		
-		int oilCnt = 1; // 기본적으로 기름이 있는곳 부터 탐색 시작 = 1개는 최소한 보장되어있다.
+	public static int countOil(int[][] land, boolean[][]isVisited, int x, int y) {
+		int oilCnt = 0;
+		
 		CoordStack cStack = new CoordStack();
+		cStack.push(x, y);
 		
 		while(true) {
 			// 남쪽 탐색
 			if(x+1 < land.length && land[x+1][y] != 0 && !isVisited[x+1][y]) {
-				cStack.push(x,y);
+				cStack.push(x+1,y);
 				isVisited[x+1][y]= true;
 			}
 			// 동쪽 탐색
 			if(y+1 < land[x].length && land[x][y+1] != 0 && !isVisited[x][y+1]) {
-				cStack.push(x,y);
+				cStack.push(x,y+1);
 				isVisited[x][y+1] = true;
 			}
 			// 북쪽 탐색
-			if(x-1 > 0 && land[x-1][y] != 0 && !isVisited[x-1][y]) {
-				cStack.push(x,y);
+			if(x-1 >= 0 && land[x-1][y] != 0 && !isVisited[x-1][y]) {
+				cStack.push(x-1,y);
 				isVisited[x-1][y] = true;
 			}
 			// 서쪽 탐색
-			if(y-1 > 0 && land[x][y-1] != 0 && !isVisited[x][y-1]) {
-				cStack.push(x,y);
+			if(y-1 >= 0 && land[x][y-1] != 0 && !isVisited[x][y-1]) {
+				cStack.push(x,y-1);
 				isVisited[x][y-1] = true;
 			}
 			
+//			System.out.println(cStack);
 			int[] nextCoord = cStack.pop();
 			if(nextCoord != null) {
+				isVisited[x][y] = true;
 				x = nextCoord[0];
 				y = nextCoord[1];
 				oilCnt++;
@@ -98,7 +137,7 @@ class CoordStack{
 	}
 	
 	public void push(int x, int y) {
-		int[] coord = { x, y };
+		int[] coord = new int[]{ x, y };
 		if(isFull())
 			increaseStackSize();
 		this.coordArr[++top] = coord;
@@ -117,7 +156,7 @@ class CoordStack{
 		for(int i=0; i <= top; i++) {
 			sb.append("[ x : ")
 			.append(coordArr[i][0])
-			.append("y : ")
+			.append(" y : ")
 			.append(coordArr[i][1])
 			.append(" ] ");
 		}
